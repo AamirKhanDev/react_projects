c5381a4a16337fbe0d22842d8d60fcbdbc1a5a8a
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2023-01-16 02:09:04 +0000

    Filled in List.js. Added in variables id,name,age,image

--- 1-birthday-reminder/src/App.js
+++ 1-birthday-reminder/src/App.js
@@ -4,11 +4,12 @@ import List from './components/List';
 
 function App() {
   const [people, setPeople] = useState(data)
+
   return (
     <main>
       <section className='container'>
-  <h3>0 Birthdays Today</h3>
-    <List />
+  <h3>{people.length} Birthdays Today</h3>
+    <List people={people}/>
     <button onClick={()=> console.log('You clicked me')}>Clear All</button>
     </section>
   </main>
